8710c61f926944fc48cfbaab87edc1abab1c624f
rashadrabby <rashadulislamrabby@gmail.com>
2022-05-21 02:03:32 +0600

    services added

--- index.js
+++ index.js
@@ -25,6 +25,28 @@ async function run() {
             res.send(services);
         });
 
+        app.get('/available', async (req, res) => {
+            const date = req.query.date || 'May 20, 2022'
+
+            //Step 1: get all services
+            const services = await serviceCollection.find().toArray();
+
+            //Step 2: get the booking of that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            //Step 3: for each service, find bookings for that service
+            services.forEach(service => {
+                const serviceBookings = bookings.filter(b => b.treatment === service.name);
+                const booked = serviceBookings.map(s => s.slot);
+                const available = service.slots.filter(s => !booked.includes(s));
+                service.available = available;
+            })
+
+            res.send(services);
+
+        });
+
         /**
          * API Naming Convention:
          * app.get('/booking') - to get all bookings in this collection or get more than one or by filter.
