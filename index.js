8338c80ecba886ccb165209388b1684e90696ec9
rashadrabby <rashadulislamrabby@gmail.com>
2022-05-21 00:54:41 +0600

    booking updated

--- index.js
+++ index.js
@@ -36,8 +36,13 @@ async function run() {
 
         app.post('/booking', async (req, res) => {
             const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists });
+            };
             const result = await bookingCollection.insertOne(booking);
-            return res.send(result);
+            return res.send({ success: true, result });
         })
 
     }
