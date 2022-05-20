063763e0a8656c4d0f138328bff767b34b1379d9
rashadrabby <rashadulislamrabby@gmail.com>
2022-05-20 17:30:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const app = express()
+const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
+const port = process.env.PORT || 7181;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqz0l.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally {
+
+    }
+
+};
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Doctors portal!')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
