3d6891a952bb3602a9a4df00c2dc6661c28666ec
rashadrabby <rashadulislamrabby@gmail.com>
2022-05-22 01:05:56 +0600

    services-edited

--- index.js
+++ index.js
@@ -48,13 +48,13 @@ async function run() {
             res.send(users);
         });
 
-        // app.put('/user/admin/:email', async (req, res) => {
-        //     const email = req.params.email;
-        //     const filter = { email: email };
-        //     const updateDoc = { $set: { role: 'admin' } };
-        //     const result = await userCollection.updateOne(filter, updateDoc);
-        //     res.send(result);
-        // });
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
