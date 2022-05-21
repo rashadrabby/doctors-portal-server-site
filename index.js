4445eb1528ce5ef9bc8a680c8a7fd16b4d32b134
rashadrabby <rashadulislamrabby@gmail.com>
2022-05-22 00:50:14 +0600

    services-edited

--- index.js
+++ index.js
@@ -43,18 +43,18 @@ async function run() {
             res.send(services);
         });
 
-        app.get('/user', async (req, res) => {
+        app.get('/user', verifyJXT, async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
         });
 
-        app.put('/user/admin/:email', async (req, res) => {
-            const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = { $set: { role: 'admin' } };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result);
-        });
+        // app.put('/user/admin/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     const filter = { email: email };
+        //     const updateDoc = { $set: { role: 'admin' } };
+        //     const result = await userCollection.updateOne(filter, updateDoc);
+        //     res.send(result);
+        // });
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
