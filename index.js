41043dada0aa78ff6d25c5d1f84feafd769cc7e1
rashadrabby <rashadulislamrabby@gmail.com>
2022-05-21 19:15:07 +0600

    services-edited

--- index.js
+++ index.js
@@ -33,11 +33,9 @@ async function run() {
             const user = req.body;
             const filter = { email: email };
             const options = { upsert: true };
-            const updateDoc = {
-                $set: user,
-            };
+            const updateDoc = { $set: user, };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
             res.send({ result, token });
         });
 
